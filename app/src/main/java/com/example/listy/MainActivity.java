fa3d1216792fb9ebc6bad3b5681d98deb3721418
bartangutan <19bartosz11@gmail.com>
2026-03-23 14:18:02 +0100

    lista na 3

--- app/src/main/java/com/example/listy/MainActivity.java
+++ app/src/main/java/com/example/listy/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.listy;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> jezykiArraylist;
+    private ArrayAdapter<String> arrayAdapter;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        jezykiArraylist = new ArrayList<>();
+        jezykiArraylist.add("Pearl");
+        jezykiArraylist.add("Ruby");
+        jezykiArraylist.add("Kotlin");
+        jezykiArraylist.add("Go");
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, jezykiArraylist);
+        listView = findViewById(R.id.listViewJezyki);
+        listView.setAdapter(arrayAdapter);
+
 
     }
 }
